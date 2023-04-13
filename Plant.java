589bc5ddea8aafc46f21cf9ddd5c7ef09aa526bb
RamiroGuzmanCh <130616535+RamiroGuzmanCh@users.noreply.github.com>
2023-04-12 22:06:36 -0500

    Create Plant.java

---
+++ Plant.java
@@ -0,0 +1,40 @@
+package Builder;
+
+public class Plant {
+	
+	private int id;
+	private int age ;
+	private int size;
+	private String color;
+	private String ScientificName;
+	private String climate;
+	private String lighPreference;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void setColor(String color ) {
+		this.color = color;
+	}
+	
+	public void setScientificName(String ScientificName) {
+		this.ScientificName = ScientificName;
+	}
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+	public void setlightPreference(String lighPreference) {
+		this.lighPreference = lighPreference;
+	}
+	
+}
+
+	
