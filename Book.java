4c711a63151d548fc63cec93721468b61f09e00d
RamiroGuzmanCh <130616535+RamiroGuzmanCh@users.noreply.github.com>
2023-04-12 22:08:37 -0500

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,38 @@
+package Builder;
+
+
+public class Book {
+	private String titulo;
+	private String isbn;
+	private String autor;
+	private String fechaPublicacion;
+	private String numeroPaginas;
+	
+
+	public void setTitulo(String titulo) {
+		this.titulo=titulo;
+		
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn=isbn;
+		
+	}
+	
+	public void setAutor(String autor) {
+		this.autor=autor;
+		
+	}
+	
+	public void setFechaPublicacion(String fechaPublicacion) {
+		this.fechaPublicacion=fechaPublicacion;
+		
+	}
+	
+	public void setNumeroPaginas(String numeroPaginas) {
+		this.numeroPaginas=numeroPaginas;
+		
+	}
+	
+
+}
